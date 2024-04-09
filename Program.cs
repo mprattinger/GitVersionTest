c0af1bcbc93316e5a7be8e9de8f32067d54f4e62
Michael Prattinger <michael.prattinger@becom-group.com>
2024-04-09 15:45:18 +0200

    trying a build

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using System.Security.Cryptography;
+
+string _version = "";
+
+var version = "1.0.0+LOCALBUILD";
+var appAssembly = typeof(Program).Assembly;
+if (appAssembly != null)
+{
+    var attrs = appAssembly.GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute));
+    if (attrs != null)
+    {
+        var infoVerAttr = (AssemblyInformationalVersionAttribute)attrs;
+        if (infoVerAttr != null && infoVerAttr.InformationalVersion.Length > 6)
+        {
+            version = infoVerAttr.InformationalVersion;
+        }
+    }
+}
+if (version.Contains('+'))
+{
+    _version = version[..version.IndexOf('+')];
+}
+else
+{
+    _version = version;
+}
+
+Console.WriteLine(_version);
